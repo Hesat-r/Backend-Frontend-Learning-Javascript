e85b8452572f1be4f86262b7a25e2d1782c431f0
Hesat Redzepi <hesatredzepi@Asits-MacBook-Air.local>
2022-06-14 12:39:34 +0200

    Clientsite Odd and Even Spliter Done with Dotcheck and only Positiv Nummbers Trying to change to Server Side

--- public/js/SplitOddAndEven.js
+++ public/js/SplitOddAndEven.js
@@ -1,31 +1,49 @@
-// buttons
+// Components
 const split = document.getElementById('Split');
 const displayOdd = document.getElementById('DisplayOdd');
 const displayEven = document.getElementById('DisplayEven');
-
+const displayNumbers = document.getElementById('DisplayNumbers');
+const buttonAddNumbers = document.getElementById('AddNumbers');
+const input = document.getElementById('inputNumbers');
 // variables
-let Numbers=[1,2,3,4,5,6,7,8,9,10];
+let Numbers=[];
 let OddNumbers=[];
 let EvenNumbers=[];
-
+//Regex to check if the input is a number witout a dot
+let regex = /^[0-9]+$/;
 // functions
 function SplitOddAndEven(Numbers) {
     for (let i = 0; i < Numbers.length; i++) {
         if (Numbers[i] % 2 == 0) {
             EvenNumbers.push(Numbers[i]);
-            console.log('Gerade Zahlen',EvenNumbers);
         } else {
             OddNumbers.push(Numbers[i]);
-            console.log('Ungerade Zahlen', OddNumbers);
         }
     }
+    return [OddNumbers, EvenNumbers];
 }
+console.log(SplitOddAndEven(Numbers));
 
-document.querySelector('#Split').onclick = (event) => {
+split.addEventListener('click',event => {
+    
    SplitOddAndEven(Numbers)
    displayOdd.innerText = OddNumbers;
    displayEven.innerText = EvenNumbers;
-   
+   displayNumbers.innerText = Numbers;
+});
+document.querySelector('#AddNumbers').onclick = (event) => {
+    if(input.value < 0){
+        alert('Sie müssen eine positive Zahl eingeben');
+        return 'Sie müssen eine positive Zahl eingeben';
+    }
+    if(regex.test(input.value)){
+        Numbers.push(input.value);
+        displayNumbers.innerText = Numbers;
+        input.value = '';
+    }
+    else{
+        alert('Sie dürfen nur Zahlen ohne Kommas eingeben');
+        return 'Sie dürfen nur Zahlen ohne Kommas eingeben';
+    }
 };
-
 
