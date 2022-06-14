19fc65c8edb57d1fb7bfd49a809474b59cce3744
Hesat Redzepi <hesatredzepi@Asits-MacBook-Air.local>
2022-06-14 16:30:15 +0200

    backend done just need to get from Frontend

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const express  = require('express');
 const app = express();
+const BodyParser = require('body-parser');
+const cors = require('cors');
+let Numbers=[];
+let OddNumbers=[];
+let EvenNumbers=[];
 
+function SplitOddAndEven(Numbers) {
+    for (let i = 0; i < Numbers.length; i++) {
+        if (Numbers[i] % 2 == 0) {
+            EvenNumbers.push(Numbers[i]);
+        } else {
+            OddNumbers.push(Numbers[i]);
+        }
+    }
+    console.log(OddNumbers,EvenNumbers);
+    return ['Ungerade Zahlen',OddNumbers,'Gerade Zahlen', EvenNumbers];
+}
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: false }));
+app.use(cors());
 app.use(express.static('public'));
-
 app.set('view engine', 'pug');
 
-app.get('/OddAndEven', (req, res) => {
+app.get('/', (req, res) => {
     res.render('./index.pug');
 });
 
-app.listen(3000);
-console.log('Server is running on port 3000');
+app.post('/',(req,res) => {
+    res.render('./index.pug');
+    Numbers.push(req.body.number);
+    console.log(Numbers);
+});
+
+app.post('/submit',(req,res) => {
+    res.render('./index.pug');
+    console.log(SplitOddAndEven(Numbers));
+});
+
+app.listen(5000);
+console.log('Server is running on port 5000');
