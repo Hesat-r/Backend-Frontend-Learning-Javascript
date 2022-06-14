2a25898801f9673ea8316ac4fb75446b8b203f3f
Hesat Redzepi <hesatredzepi@Asits-MacBook-Air.local>
2022-06-14 10:53:42 +0200

    Frontend Base not Done

---
+++ index.js
@@ -0,0 +1,12 @@
+const express  = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.set('view engine', 'pug');
+app.get('/', (req, res) => {
+    res.render('./index.pug');
+});
+
+app.listen(3000);
+console.log('Server is running on port 3000');
