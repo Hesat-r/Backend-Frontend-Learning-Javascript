093dde188f8ad3e4e9e70dc478949b14bc3e9247
Hesat Redzepi <hesatredzepi@Asits-MacBook-Air.local>
2022-06-17 11:24:28 +0200

    moved schemas folder into database folder and created first route but with error on search api

--- index.js
+++ index.js
@@ -2,37 +2,20 @@ const express  = require('express');
 const app = express();
 const BodyParser = require('body-parser');
 const cors = require('cors');
-const mongo = require('./js/MongoCon.js');
-const NumberSchema = require('./Schemas/Number-schema.js');
-let Numbers=[];
-let OddNumbers=[];
-let EvenNumbers=[];
+const mongo = require('./Database/MongoCon.js');
+const SplitOddAndEvenRoute = require('./routes/SplitOddAndEvenRoute.js');
 
 
 
-function SplitOddAndEven(Numbers) {
-    for (let i = 0; i < Numbers.length; i++) {
-        if (Numbers[i] % 2 == 0) {
-            EvenNumbers.push(' '+ Numbers[i] + ' ');
-        } else {
-            OddNumbers.push(' '+ Numbers[i] + ' ');
-        }
-    }
-    return ['Ungerade Zahlen',OddNumbers,'Gerade Zahlen', EvenNumbers];
-}
+
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static('public'));
 app.set('view engine', 'pug');
 
-const number = {
-    numbers: Numbers,
-    oddNumbers: OddNumbers,
-    evenNumbers: EvenNumbers
-    
-}
+
 const connectToMongoDB = async() => {
     await mongo().then(async (mongoose) => {
         try{
@@ -45,51 +28,16 @@ const connectToMongoDB = async() => {
   })
 }
 connectToMongoDB();
-app.get('/SplitOddAndEven', (req, res) => {
-    res.render('./SplitOddAndEven.pug');
-});
+
+app.use('/SplitOddAndEven', SplitOddAndEvenRoute)
+
 app.get('/', (req, res) => {
     res.render('./index.pug');
 });
-app.post('/SplitOddAndEven',(req,res) => {
-    res.render('./SplitOddAndEven.pug');
-    if(req.body.number < 0){
-        return 'Sie müssen eine positive Zahl eingeben';
-    }
-    if(/^[0-9]+$/.test(req.body.number)){
-        Numbers.push(' '+req.body.number + ' ');
-    }
-    
-    console.log(Numbers);
-});
 
-app.post('/SplitOddAndEven/submit', async(req,res) => {
-    res.render('./SplitOddAndEven.pug');
-    console.log(SplitOddAndEven(Numbers));
-    await  new NumberSchema(number).save();
-});
-app.get('/SplitOddAndEven/api',(req,res) => {
-    res.json({"Numbers":Numbers ,"OddNumbers":OddNumbers, "EvenNumbers":EvenNumbers});
-    
-});
 
-app.get('/SplitOddAndEven/api/:id',(req,res) =>{
-    fetchid = req.params.id;
-    NumberSchema.findById(fetchid).then((result) => {
-        res.send(result)
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-    
-});
-app.post('/SplitOddAndEven/delete',(req,res) => {
-    res.render('./SplitOddAndEven.pug');
-    Numbers.length = 0;
-    OddNumbers.length = 0;
-    EvenNumbers.length = 0;
-    console.log(Numbers);
-    });
 
 app.listen(5000);
 console.log('Server is running on port 5000');
+
+module.exports = app;
---
+++ routes/SplitOddAndEvenRoute.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = express.Router();
+const NumberSchema = require('/Users/hesatredzepi/Desktop/SplittOddAndEven/Database/Schemas/Number-schema.js');
+let Numbers=[];
+let OddNumbers=[];
+let EvenNumbers=[];
+
+function SplitOddAndEven(Numbers) {
+    for (let i = 0; i < Numbers.length; i++) {
+        if (Numbers[i] % 2 == 0) {
+            EvenNumbers.push(' '+ Numbers[i] + ' ');
+        } else {
+            OddNumbers.push(' '+ Numbers[i] + ' ');
+        }
+    }
+    return ['Ungerade Zahlen',OddNumbers,'Gerade Zahlen', EvenNumbers];
+}
+
+const number = {
+    numbers: Numbers,
+    oddNumbers: OddNumbers,
+    evenNumbers: EvenNumbers
+    
+}
+
+router.get('/', (req, res,next) => {
+    res.render('./SplitOddAndEven.pug');
+    next();
+});
+
+router.post('/addnumber',(req,res,next) => {
+    res.render('./SplitOddAndEven.pug');
+    if(req.body.number < 0){
+        return 'Sie müssen eine positive Zahl eingeben';
+    }
+    if(/^[0-9]+$/.test(req.body.number)){
+        Numbers.push(' '+req.body.number + ' ');
+    }
+    
+    console.log(Numbers);
+    next();
+});
+
+router.post('/submit', async(req,res,next) => {
+    res.render('./SplitOddAndEven.pug');
+    console.log(SplitOddAndEven(Numbers));
+    await  new NumberSchema(number).save();
+    next();
+});
+
+router.get('/api',(req,res,next) => {
+    res.json({"Numbers":Numbers ,"OddNumbers":OddNumbers, "EvenNumbers":EvenNumbers});
+   next();
+});
+
+router.get('/api/:_id',(req,res,next) =>{
+    fetchid = req.params._id;
+    NumberSchema.findById(fetchid).then((result) => {
+        res.send(result)
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+   next();
+});
+router.post('/delete',(req,res) => {
+    res.render('./SplitOddAndEven.pug');
+    Numbers.length = 0;
+    OddNumbers.length = 0;
+    EvenNumbers.length = 0;
+    console.log(Numbers);
+    });
+
+    module.exports = router;
