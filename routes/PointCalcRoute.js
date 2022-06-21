bcf9d5bf347fcfa5bc68f4960a216d712692e89c
Hesat Redzepi <hesatredzepi@Asits-MacBook-Air.local>
2022-06-21 11:08:39 +0200

    backup point

--- routes/PointCalcRoute.js
+++ routes/PointCalcRoute.js
@@ -8,27 +8,44 @@ router.get('/', (req, res,next) => {
     next();
 });
 
-router.post('/', (req, res,next) => {
 
-    if(req.body.number1 === '' || req.body.number2 === ''){
+function Calculator(number1,number2) {
+    if(number1 === '' || number2 === ''){
         calc = 'You need to add a Number for Number 1 and Number 2';
        
-    }else if(req.body.number1 === req.body.number2){
+    }else if(number1 === number2){
         calc = 1;
-    }else if(req.body.number1 > req.body.number2){
+    }else if(number1 > number2){
         calc = 3;
-    }else if(req.body.number1 < req.body.number2){
+    }else if(number1 < number2){
         calc = 0;
     }
     if(result === NaN){
         result = 1;
     }
-    values.push(req.body.number1 + ' : ' + req.body.number2);
+    values.push(number1 + ' : ' + number2);
 
     result = calc + result;
     console.log(result);
     console.log(values);
+}
+
+router.post('/', (req, res,next) => {
+    Calculator(req.body.number1,req.body.number2);
     res.render('./PointCalc.pug', { result: result });
+    next();
+});
+
+
+
+
+router.post('/delete', (req, res,next) => {
+    result = 0;
+    calc = 0;
+    values = [];
+    req.body.number1 = 0;
+    req.body.number2 = 0;
+    res.render('./PointCalc.pug');
     next();
     });
 
