1370c317f174e7e85e35bcd57e4e2394eacca7e7
Hesat Redzepi <hesatredzepi@Asits-MacBook-Air.local>
2022-06-22 17:39:27 +0200

    Frontend done showing result

--- routes/AbbreviateNameRoute.js
+++ routes/AbbreviateNameRoute.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const Abbreviate = require('../lib/AbbreviateName');
+let result;
 router.get('/', (req,res,next) => {
     res.render('./AbbreviateName.pug');
     next();
 });
 
 router.post('/', (req,res,next) => {
-    console.log(Abbreviate.Abbreviate(req.body.input));
-    res.render('./AbbreviateName.pug');
+    result = Abbreviate.Abbreviate(req.body.input);
+    res.render('./AbbreviateName.pug', { result : result});
     next();
 });
 
