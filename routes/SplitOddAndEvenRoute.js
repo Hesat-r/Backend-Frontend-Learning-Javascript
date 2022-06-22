347c304017844b67975286ae89df7992e4383557
Hesat Redzepi <hesatredzepi@Asits-MacBook-Air.local>
2022-06-22 11:42:50 +0200

    SplitOddAndEvenLib implemented needed some changes

--- routes/SplitOddAndEvenRoute.js
+++ routes/SplitOddAndEvenRoute.js
@@ -1,51 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const NumberSchema = require('/Users/hesatredzepi/Desktop/SplittOddAndEven/Database/Schemas/Number-schema.js');
-let Numbers=[];
-let OddNumbers=[];
-let EvenNumbers=[];
-
-
-function SplitOddAndEven(Numbers) {
-    for (let i = 0; i < Numbers.length; i++) {
-        if (Numbers[i] % 2 == 0) {
-            EvenNumbers.push(' '+ Numbers[i] + ' ');
-        } else {
-            OddNumbers.push(' '+ Numbers[i] + ' ');
-        }
-    }
-    return ['Ungerade Zahlen',OddNumbers,'Gerade Zahlen', EvenNumbers];
-}
-
-const number = {
-    numbers: Numbers,
-    oddNumbers: OddNumbers,
-    evenNumbers: EvenNumbers
-    
-}
-
+const spliter = require('../lib/SplitOddAndEven');
+let values;
+let Odd;
+let Even;
+let numbers;;
 router.get('/', (req, res,next) => {
     res.render('./SplitOddAndEven.pug');
     next();
 });
 
 router.post('/addnumber',(req,res,next) => {
+    Numbers = spliter.addnumbers(req.body.number);
     res.render('./SplitOddAndEven.pug');
-    if(req.body.number < 0){
-        return 'Sie müssen eine positive Zahl eingeben';
-    }
-    if(/^[0-9]+$/.test(req.body.number)){
-        Numbers.push(' '+req.body.number + ' ');
-    }
-    
-    console.log(Numbers);
     next();
 });
 
 router.post('/submit', async(req,res,next) => {
     res.render('./SplitOddAndEven.pug');
-    console.log(SplitOddAndEven(Numbers));
-    await  new NumberSchema(number).save();
+     values = spliter.split();
+     Odd = values[0];
+     Even = values[1];
+     numbers = values[2];
+    console.log('ODD Number : ' + Odd + ' EvenNumbers ' + Even+ ' Numbers ' + numbers);
     next();
 });
 
@@ -60,16 +38,19 @@ router.get('/api/:id',async (req,res,next) =>{
         console.log('ID ROUTE ' + fetchid);
         next();
 });
+
 router.get('/api',(req,res,next) => {
-    res.json({"Numbers":Numbers ,"OddNumbers":OddNumbers, "EvenNumbers":EvenNumbers});
+    res.json({"Numbers":numbers ,"OddNumbers":Odd, "EvenNumbers":Even});
    next();
 });
+
 
 router.post('/delete',(req,res) => {
     res.render('./SplitOddAndEven.pug');
     Numbers.length = 0;
     OddNumbers.length = 0;
     EvenNumbers.length = 0;
+    spliter.delete();
     console.log(Numbers);
     });
 
